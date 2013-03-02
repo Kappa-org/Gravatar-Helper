f8d147df08be83dc1123baa6121097cf803669c7
Budry <zarubaondra@gmail.com>
2013-03-02 13:49:08 +0100

    Added helper

--- Kappa/Templating/Helpers/GravatarHelper.php
+++ Kappa/Templating/Helpers/GravatarHelper.php
@@ -14,9 +14,63 @@ use Nette;
 
 class GravatarHelper extends Nette\Object
 {
-	public function gravatar($email)
+	const GRAVATAR_URL = "http://www.gravatar.com/avatar/";
+
+	/** @var string */
+	private $gravatarDefault;
+
+	/** @var int */
+	private $size;
+
+	/** @var string */
+	private $emailHash;
+
+	/**
+	 * @param null $gravatarDefault
+	 * @throws \Kappa\FileNotFoundException
+	 */
+	public function __construct($gravatarDefault = null)
 	{
-		dump($email);
-		die();
+		if($gravatarDefault !== null && !file_exists($gravatarDefault))
+			throw new  \Kappa\FileNotFoundException(__CLASS__, $gravatarDefault);
+		$this->gravatarDefault = urlencode($gravatarDefault);
+	}
+
+	/**
+	 * @param string $email
+	 * @param int $size
+	 * @return string
+	 */
+	public function gravatar($email, $size)
+	{
+		$this->emailHash = $this->createEmailHash($email);
+		$this->size = $size;
+		return $this->createUrl();
+	}
+
+	/**
+	 * @param string $email
+	 * @return string
+	 */
+	private function createEmailHash($email)
+	{
+		$email = trim($email);
+		$email = strtolower($email);
+		$email = md5($email);
+		return $email;
+	}
+
+	/**
+	 * @return string
+	 */
+	private function createUrl()
+	{
+		$url = self::GRAVATAR_URL;
+		$url .= $this->emailHash;
+		$url .= '?d=';
+		$url .= $this->gravatarDefault;
+		$url .= '&s=';
+		$url .= $this->size;
+		return $url;
 	}
 }
