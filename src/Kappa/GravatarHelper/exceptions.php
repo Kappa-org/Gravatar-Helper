0b0ac347fd438acc08dd3e25b1c9e3190fa0a37c
Budry <zarubaondra@gmail.com>
2014-05-21 16:52:00 +0200

    Added FileNotFound exception

---
+++ src/Kappa/GravatarHelper/exceptions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Kappa\GravatarHelper package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\GravatarHelper;
+
+/**
+ * Class FileNotFoundException
+ * @package Kappa\GravatarHelper
+ */
+class FileNotFoundException extends \LogicException
+{
+
+}
