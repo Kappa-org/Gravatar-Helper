79619302d28e18a60275bbdfd174838ff5261a5d
Budry <zarubaondra@gmail.com>
2014-05-21 21:45:13 +0200

    Added cache storage for faster display avatar

---
+++ src/Kappa/GravatarHelper/CacheStorage.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * This file is part of the Kappa\GravatarHelper package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\GravatarHelper;
+
+use Kappa\FileSystem\File;
+use Nette\Object;
+use Nette\Utils\DateTime;
+
+/**
+ * Class CacheStorage
+ * @package Kappa\GravatarHelper
+ */
+class CacheStorage extends Object
+{
+	/** @var string */
+	private $tempDirectory;
+
+	/**
+	 * @param string $path
+	 * @throws DirectoryNotFoundException
+	 */
+	public function __construct($path)
+	{
+		$path = realpath($path);
+		if (!$path) {
+			throw new DirectoryNotFoundException("Directory '{$path}' has not been found");
+		}
+		$this->tempDirectory = $path;
+	}
+
+	/**
+	 * @param string $url
+	 * @return File
+	 */
+	public function getAvatarCache($url)
+	{
+		return $this->invalidateAvatarCache($url);
+	}
+
+	/**
+	 * @param string $url
+	 * @return File
+	 */
+	private function invalidateAvatarCache($url)
+	{
+		$headers = get_headers($url);
+		$lastUpdate = str_replace('Last-Modified: ', '', $headers[8]);
+		$datetime = new DateTime($lastUpdate);
+		$cacheFile = $this->getCachePath($url);
+		if (is_file($cacheFile)) {
+			$file = File::open($cacheFile);
+			if ($datetime->getTimestamp() > $file->getInfo()->getMTime()) {
+				return $this->createAvatarCache($url);
+			} else {
+				return $file;
+			}
+		} else {
+			return $this->createAvatarCache($url);
+		}
+	}
+
+	/**
+	 * @param string $url
+	 * @return File
+	 */
+	private function createAvatarCache($url)
+	{
+		$file = File::fromUrl($url, $this->getCachePath($url));
+
+		return $file;
+	}
+
+	/**
+	 * @param string $url
+	 * @return string
+	 */
+	private function getCachePath($url)
+	{
+		return $this->tempDirectory . DIRECTORY_SEPARATOR . $this->getCacheName($url) . '.png';
+	}
+
+	/**
+	 * @param string $url
+	 * @return string
+	 */
+	private function getCacheName($url)
+	{
+		return md5($url);
+	}
+} 
---
+++ tests/Kappa/GravatarHelper/CacheStorageTest.phpt
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of the Kappa\GravatarHelper package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ * 
+ * @testCase
+ */
+
+namespace Kappa\GravatarHelper\Tests;
+
+use Kappa\GravatarHelper\CacheStorage;
+use Kappa\Tester\TestCase;
+use Tester\Assert;
+use Tester\Helpers;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+/**
+ * Class CacheStorage
+ * @package Kappa\GravatarHelper\Tests
+ */
+class CacheStorageTest extends TestCase
+{
+	/** @var \Kappa\GravatarHelper\CacheStorage */
+	private $cacheStorage;
+	
+	/** @var string */
+	private $tempDirectory;
+
+	protected function setUp()
+	{
+		$tempDirectory = __DIR__ . '/../../data/';
+		if (!is_dir($tempDirectory)) {
+			mkdir($tempDirectory);
+		}
+		$this->tempDirectory = realpath($tempDirectory);
+		$this->cacheStorage = new CacheStorage($tempDirectory);
+	}
+
+	public function testGetAvatarCache()
+	{
+		$emailHash = md5('test@test.com');
+		$url = 'http://www.gravatar.com/avatar/http://www.gravatar.com/avatar/' . $emailHash;
+		$urlHash = md5($url);
+		$cacheFile = $this->tempDirectory . DIRECTORY_SEPARATOR . $urlHash . '.png';
+		Assert::false(is_file($cacheFile));
+		Assert::type('Kappa\FileSystem\File', $this->cacheStorage->getAvatarCache($url));
+		Assert::true(is_file($cacheFile));
+	}
+
+	protected function tearDown()
+	{
+		Helpers::purge($this->tempDirectory);
+	}
+}
+
+\run(new CacheStorageTest());
